6c460956a67e7578e36b118b49da583cb495b1ee
kristohi <32636097+kristohi@users.noreply.github.com>
2018-03-21 12:56:37 +0100

    Add files via upload

---
+++ Arduino-Nano-LinearRobot/Arduino-Nano-LinearRobot.ino
@@ -0,0 +1,90 @@
+#include <Stepper.h>
+
+
+#define MOTOR_ENABLE 8
+#define X_MOTOR_STEP 2
+#define X_MOTOR_DIR 5
+#define X_HOME_SWITCH 12
+#define X_AWAY_SWITCH 9
+#define Y_MOTOR_STEP 3
+#define Y_MOTOR_DIR 6
+#define Y_HOME_SWITCH 10
+#define Y_AWAY_SWITCH 11
+#define DIR_LEFT -1
+#define DIR_RIGHT 1
+
+#define STEP_PER_REVOLUTION 400
+#define DRIVER_MICROSTEPS 0.25
+#define BELT_PRESENT_PITCH 3 //mm 
+#define PULLEY_TOOTH 20 
+
+float stepPerMM = 0; 
+bool cal = true;
+int stepSpeed = 450;
+int travelledDistance; 
+
+typedef struct Pos{
+  int x;
+  int y;
+ }Pos;
+ 
+Pos currentPos;
+Pos maxPos;
+Pos newPos;
+
+Stepper motorX(STEP_PER_REVOLUTION, X_MOTOR_STEP, X_MOTOR_DIR);
+Stepper motorY(STEP_PER_REVOLUTION, Y_MOTOR_STEP, Y_MOTOR_DIR);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(X_HOME_SWITCH, INPUT_PULLUP);
+  pinMode(Y_HOME_SWITCH, INPUT_PULLUP);
+  pinMode(Z_HOME_SWITCH, INPUT_PULLUP);
+  pinMode(X_AWAY_SWITCH, INPUT_PULLUP);
+  pinMode(Y_AWAY_SWITCH, INPUT_PULLUP); 
+  pinMode(Z_AWAY_SWITCH, INPUT_PULLUP);
+  pinMode(MOTOR_ENABLE, HIGH);
+  stepPerMM = stepsToMMCalculation(STEP_PER_REVOLUTION, DRIVER_MICROSTEPS, BELT_PRESENT_PITCH, PULLEY_TOOTH);
+
+
+  currentPos.x = 0;
+  currentPos.y = 0;
+  maxPos.x = 5000;
+  maxPos.y = 3000;
+  
+  newPos.y = 0;
+  newPos.x = 0;
+}
+
+void loop()
+{
+  if (!cal)
+  { 
+    calibration();
+    cal = true;
+  }
+  
+  newPos.x = 50;
+  newPos.y = 75;
+  
+
+  
+  while(moveStep(newPos.x,newPos.y))
+  {
+      Serial.println("Trying move");
+    //Serial.println("Current x pos " + currentPos.x);
+    Serial.println(currentPos.x);
+    Serial.println(currentPos.y);
+    //Serial.println("Current y pos " + currentPos.y);
+    delay(100);
+  }
+
+//  Serial.print("travelled distance x direction: ");
+//  Serial.print(maxPosXinMM); //xCoordinate
+//  Serial.println("travelled distance y direction:  ");
+//  Serial.print(maxPosYinMM); //yCoordinate
+}
+
+
+
+
---
+++ Arduino-Nano-LinearRobot/MovingFunctions.ino
@@ -0,0 +1,81 @@
+/**
+   Slope of a line can be expressed as deltaX/deltaY. A number of steps on Y equals a number of steps on X.
+   This will help us step along Y and adds deltaX to a counter, when the counter >=Y steps a number on X.'
+   The motors will toggle simultaniously to the newX and newY. 
+*/
+
+boolean moveStep(int newX, int newY) {
+  int s = 0;
+  float calcSteep = 0;
+  int deltaX = newX - currentPos.x; //distance to move
+  int deltaY = newY - currentPos.y;
+
+  //Return bool to indicate if the steppers still need to move to reach position
+  boolean stillMoving = false;
+
+
+  //Serial.println("Current x pos " + currentPos.x);
+
+  //Check if X should move at all
+  if (deltaX != 0 && deltaY != 0)
+  {
+    //Check if x should move more then Y
+    if (deltaX > deltaY)
+    {
+      calcSteep = deltaX / deltaY;
+      
+     s = round(calcSteep);
+     
+      //Do the moving
+      motorX.step(dirX()*s); //onestep and direction
+      motorY.step(dirY()); //onestep and direction
+
+       //Increment currentpos
+      currentPos.x = (currentPos.x + (dirX()*s));
+      currentPos.y = (currentPos.y + dirY());
+    }
+
+    //Check if Y should move more then X
+    else if (deltaY > deltaX) {
+      
+      calcSteep = deltaY / deltaX; 
+      s = round(calcSteep);
+       //Move the steppers
+      motorY.step(dirY()*s); //onestep and direction
+      motorX.step(dirX());
+
+       //Increment currentpos
+      currentPos.x = (currentPos.x + dirX());
+      currentPos.y = (currentPos.y + (dirY()*s));
+    }
+
+    //Set the return bool to indicate further actions are required
+    stillMoving = true;
+  }
+  //IF one of the steppers have reached position
+  //Check if one of the steppers still need to move
+  //Check stepper X
+  else if (deltaX != 0)
+  {
+
+    motorX.step(dirX()*deltaX); //onestep and direction
+    currentPos.x = (currentPos.x + dirX()*deltaX);
+  }
+  //Check if stepper Y needs to move
+  else if (deltaY != 0)
+  {
+    motorX.step(dirY()*deltaY); //onestep and direction
+     currentPos.y = (currentPos.y + dirY()*deltaY);
+  }
+
+
+
+  //Return the bool which indicated if position is reached or not
+  return stillMoving;
+
+
+}
+
+/******************************************************/
+
+
