9f775e89001e2cc01cdad1300f8744af804eff43
yngbra94 <yngbra94@gmail.com>
2018-04-24 14:47:41 +0200

    Add files via upload

---
+++ Arduino-Nano-LinearRobot_Y3/Calculations.ino
@@ -0,0 +1,91 @@
+
+/*Different calculations for the RoeBot*/
+
+int calculateDistance(int X0, int X1, int Y0, int Y1) {
+  long calculatedDistance;
+  int deltaX = X1 - X0;
+  int deltaY = Y1 - Y0;
+  calculatedDistance = sqrt(sq(deltaX) + sq(deltaY));
+  return calculatedDistance;
+}
+/******************************************************/
+int calculateQuotient(int X0, int X1, int Y0, int Y1, int Z0, int Z1) {
+  long calculatedQuotient;
+  int deltaX = X1 - X0;
+  int deltaY = Y1 - Y0;
+  //int deltaZ = Z1-Z0;
+  calculatedQuotient = (deltaX / deltaY);
+  return calculatedQuotient;
+}
+
+/** Calculates mm input to step output for given axis.
+
+*/
+int MMToSteps(float millimeter, int axis) {
+  int steps = 0;
+  if (axis == X_AXIS) {
+    steps = X_STEP_PER_MM * millimeter;
+  } else if (axis == Y_AXIS) {
+    steps = X_STEP_PER_MM * millimeter;
+  }
+  return steps;
+
+}
+
+
+
+
+/******************************************************/
+float stepsPerMM(int stepsPerRev, float driverMicrosteps, int belt_pitch, int pulley_teeth) {
+  float result;
+  result = ((stepsPerRev * driverMicrosteps) / (belt_pitch * pulley_teeth));
+  return result;
+}
+
+float MMPerSteps(int belt_pitch, int pulley_teeth, int stepsPerRev, float driverMicrosteps) {
+  float result;
+  result = ((belt_pitch * pulley_teeth) / (stepsPerRev * driverMicrosteps));
+  return result;
+}
+/******************************************************/
+//Find the direction for x movement
+int dirX() {
+  int directionX;
+
+  if (currentPos.x > newPos.x)
+  {
+    directionX = DEC_DIR;
+  }
+  else if (currentPos.x < newPos.x)
+  {
+    directionX = INC_DIR;
+  }
+  else if (currentPos.x == newPos.x)
+  {
+    directionX = 0;
+  }
+  return directionX;
+}
+
+/******************************************************/
+//Find the direction for y movement
+int dirY() {
+  int directionY;
+  if (currentPos.y > newPos.y)
+  {
+    directionY = DEC_DIR;
+  }
+  else if (currentPos.y < newPos.y)
+  {
+    directionY = INC_DIR;
+  }
+  else if (currentPos.y == newPos.y)
+  {
+    directionY = 0;
+  }
+  return directionY;
+}
+
+
+
+
---
+++ Arduino-Nano-LinearRobot_Y3/EndStopConverter.ino
@@ -0,0 +1,27 @@
+// Converts an analog innput value to an bool.
+// To be used for digital switches connected to analog inputs. (Analog to digital converter) .
+bool ATDConverter(short value) {
+  bool val = false;
+  if (analogRead(value) >= 512) {
+    val = true;
+  } else {
+    val = false;
+  }
+  return val;
+}
+
+// Prints the status og all endstops.
+void printEndstopsString() {
+  Serial.print("Magnet Endstop:  ");
+  Serial.println(ATDConverter(MAGNET_ENDSTOP));
+  Serial.print("X home:  ");
+  Serial.println(ATDConverter(X_HOME_SWITCH));
+  Serial.print("X Away:  ");
+  Serial.println(ATDConverter(X_OUTER_SWITCH));
+  Serial.print("Y home:  ");
+  Serial.println(ATDConverter(Y_HOME_SWITCH));
+  Serial.print("Y Away:  ");
+  Serial.println(ATDConverter(Y_OUTER_SWITCH));
+
+}
+
