413f4615251a3c54570cceb6231e20f17a05c878
yngbra94 <yngbra94@gmail.com>
2018-04-25 16:09:13 +0200

    Add files via upload

---
+++ Arduino-Nano-LinearRobot_Y4/MotorControl.ino
@@ -0,0 +1,19 @@
+/**
+   FUNCTIONS TO CONTROL THE MOTORS
+*/
+
+void enableMotors()
+{
+  digitalWrite(MOTOR_ENABLE, LOW);
+}
+
+void disableMotors()
+{
+  digitalWrite(MOTOR_ENABLE, HIGH);
+}
+
+
+
+
+
+
