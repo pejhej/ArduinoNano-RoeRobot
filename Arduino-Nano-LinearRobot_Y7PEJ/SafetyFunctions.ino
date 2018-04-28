325a14c072bc0e0143951ee079b7ab7f20eb5a6b
pejhej <36162293+pejhej@users.noreply.github.com>
2018-04-28 14:26:00 +0000

    Add files via upload

---
+++ Arduino-Nano-LinearRobot_Y7PEJ/SafetyFunctions.ino
@@ -0,0 +1,98 @@
+
+
+
+/**
+    This function is called when one of the limit switches are triggered
+*/
+/*
+  void xHomeLimitTrig()
+  {
+  interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200)
+  {
+     xHomeSwitch = toggleBool(xHomeSwitch);
+  }
+  last_interrupt_time = interrupt_time;
+  }
+*/
+
+
+
+void stopMotors() {
+  mainCommand = stopRobot;
+}
+
+
+
+/**
+   Toggle the bool given to this function
+*/
+boolean toggleBool(boolean toggleThis)
+{
+
+  //Check if its high
+  if (toggleThis)
+  {
+    toggleThis = false;
+  }
+  else if (!toggleThis)
+  {
+    toggleThis = true;
+  }
+
+  return toggleThis;
+}
+
+
+/**
+   Checks the limit switches and sets the appropriate bools
+*/
+bool limitTrigged()
+{
+  bool limitIsTrigged = false;
+
+
+  if (!digitalRead(X_HOME_SWITCH))
+  {
+    xHomeSwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    xHomeSwitch = false;
+  }
+  if (!digitalRead(X_OUTER_SWITCH))
+  { xAwaySwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    xAwaySwitch = false;
+  }
+  if (!digitalRead(Y_HOME_SWITCH))
+  { yHomeSwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    yHomeSwitch = false;
+  }
+  if (!digitalRead(Y_OUTER_SWITCH))
+  {
+    yAwaySwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    yAwaySwitch = false;
+  }
+
+
+  return limitIsTrigged;
+}
+
+
+
+
+
---
+++ Arduino-Nano-LinearRobot_Y7PEJ/addOnFunctions.ino
@@ -0,0 +1,65 @@
+
+
+
+
+  /**
+   Check failure and return the given failure state
+*/
+state checkFailure()
+{
+  state returnState = fail;
+
+  if (xHomeSwitch || xAwaySwitch || yHomeSwitch || yAwaySwitch )
+  {
+    inState = linearBotLimitTrigged;
+  }
+
+  return returnState;
+}
+/**
+   Returns true if the robot is in same position as new-position. Ergo no moving required
+*/
+boolean robotInPosition()
+{
+  boolean inPos = false;
+  if ((currentPos.x == newPos.x) && (currentPos.y == newPos.y))
+  {
+    inPos = true;
+  }
+
+  return inPos;
+}
+
+/**
+ * Update the status to given status and notify over serial com
+ */
+void updateStatusAndNotify(byte state) {
+  int readyCounter = 1;
+  if (inState != state) {
+    inState = state;
+    sendInt(inState);
+    //Counter for making sure the controller updates the ready to recieve status - send it multiple times
+    if(state == readyToRecieve)
+    {
+      for(int i=0; i<readyCounter; ++i)
+      {
+        sendInt(inState);
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
